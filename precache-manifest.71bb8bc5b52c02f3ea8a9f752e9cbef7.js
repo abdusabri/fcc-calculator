a812a787466c1bc8207832af186741cc811c9791
Abdu Assabri <abdusabri@outlook.com>
2018-12-23 16:57:45 +0100

    Updates

---
+++ precache-manifest.71bb8bc5b52c02f3ea8a9f752e9cbef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87070cd8d4bae05658c4",
+    "url": "/fcc-calculator/static/js/runtime~main.87070cd8.js"
+  },
+  {
+    "revision": "57e56e7b0b1528c8ae38",
+    "url": "/fcc-calculator/static/js/main.57e56e7b.chunk.js"
+  },
+  {
+    "revision": "f0914a8c4f65bea29905",
+    "url": "/fcc-calculator/static/js/1.f0914a8c.chunk.js"
+  },
+  {
+    "revision": "57e56e7b0b1528c8ae38",
+    "url": "/fcc-calculator/static/css/main.dfbed578.chunk.css"
+  },
+  {
+    "revision": "9c725fb67a99d39e4049ecd84c77cc96",
+    "url": "/fcc-calculator/index.html"
+  }
+];
